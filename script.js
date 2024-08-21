fd00c4398ecb9860ec17bdb9e34c4aa756cb3377
rencygisac <rencygisac@gmail.com>
2024-08-21 17:24:53 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+
+document.write("Hello World");
+document.write("<br>")
+
+document.write("Hello World" + "<br>");
+document.write("Hello World");
+console.log("This is a test message");
+// console.warn("warning message");
+// console.error("error message");
+
+// this is a comment
+
+/*this is a comment
+hello
+good morning */
+
+// let var const
+
+// let person = "Anie"
+var person = "Anie"
+
+var age = 19;
+
+console.log(person);
+console.log(age);
+
+// const pi=3.14; cant be reassigned
+
+console.log(a);
+var a = 10;
+
+console.log(b);
+let b=10;
+
+
